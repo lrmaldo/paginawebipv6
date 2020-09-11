4ef4f63e34a07e9d02b62b8be71e82cd4f1d22ed
leo <lrmaldo@gmail.com>
2020-09-11 11:01:03 -0500

    commando 2

--- index.php
+++ index.php
@@ -342,6 +342,28 @@ echo $_SERVER['SERVER_ADDR'];
 $command="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
 $localIP = exec ($command);
 echo $localIP;
+
+echo "<br>";
+function get_local_ipv4() {
+    $out = split(PHP_EOL,shell_exec("/sbin/ifconfig"));
+    $local_addrs = array();
+    $ifname = 'unknown';
+    foreach($out as $str) {
+      $matches = array();
+      if(preg_match('/^([a-z0-9]+)(:\d{1,2})?(\s)+Link/',$str,$matches)) {
+        $ifname = $matches[1];
+        if(strlen($matches[2])>0) {
+          $ifname .= $matches[2];
+        }
+      } elseif(preg_match('/inet addr:((?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3})\s/',$str,$matches)) {
+        $local_addrs[$ifname] = $matches[1];
+      }
+    }
+    return $local_addrs;
+  }
+  
+  $addrs = get_local_ipv4();
+  var_export($addrs);
 ?>
 <pre id="details"></pre>
                             </div>

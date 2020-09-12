e85a7936f6e2e31900a6d702072499243e08703d
Ing Leonardo <lrmaldo@gmail.com>
2020-09-11 21:02:45 -0500

    ip v1.0.0

--- index.php
+++ index.php
@@ -273,7 +273,15 @@
                             <div class="text-white">
                                 Mi IP es: <strong id="ipId" class="text-white"></strong>
                                 <br>
-                            <?php  echo $_SERVER['REMOTE_ADDR'] ?>
+                            <?php
+                            if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARTDED_FOR'] != '') {
+                                $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                            } else {
+                                $ip_address = $_SERVER['REMOTE_ADDR'];
+                            }  
+                            
+                            
+                            echo  $ip_address; ?>
                             <br>
                            
                             </div>
